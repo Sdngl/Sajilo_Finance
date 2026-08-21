1ce03ddc3068a90c7b08490b6c5e0f5343fbff51
Sdngl <sdngl12345@gmail.com>
2026-08-21 10:40:42 +0545

    first commit

---
+++ app/dashboard/literacy/page.tsx
@@ -0,0 +1,6 @@
+import {DashboardShell} from '../../../components/layout/DashboardShell';
+import DashboardContent from '../../../components/dashboard/DashboardContent';
+
+export default function FinancialLiteracyPage(){
+  return <DashboardShell><DashboardContent/></DashboardShell>;
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,1 @@
+import {DashboardShell} from '../../components/layout/DashboardShell';import DashboardContent from '../../components/dashboard/DashboardContent';export default function Page(){return <DashboardShell><DashboardContent/></DashboardShell>}
